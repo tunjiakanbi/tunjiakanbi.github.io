16380d763d22e028e55f99622c61806ffcc8354a
tunjiakanbi <tunjiakanbi@gmail.com>
2018-07-11 23:18:08 -0500

    added tunjiPortfolio

---
+++ tunjiPortfolio/assets/js/main.js
@@ -0,0 +1,164 @@
+$(document).ready(function(){
+    
+	// Init ScrollMagic
+	var controller = new ScrollMagic.Controller();
+
+	// build a scene
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '#project01'
+	})
+	.setClassToggle('#project01', 'fade-in') // add class to project01
+	// .addIndicators({
+	// 	name: 'fade scene',
+	// 	colorTrigger: 'black',
+	// 	indent: 200,
+	// 	colorStart: '#75C695'
+	// }) // this requires a plugin
+	.addTo(controller);
+
+
+	var controller = new ScrollMagic.Controller();
+
+	// build a scene
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '#project02'
+	})
+	.setClassToggle('#project02', 'fade-in') // add class to project01
+	// .addIndicators({
+	// 	name: 'fade scene',
+	// 	colorTrigger: 'black',
+	// 	indent: 200,
+	// 	colorStart: '#75C695'
+	// }) // this requires a plugin
+	.addTo(controller);
+
+	
+	
+	var controller = new ScrollMagic.Controller();
+
+	// build a scene
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '#project03'
+	})
+	.setClassToggle('#project03', 'fade-in') // add class to project01
+	// .addIndicators({
+	// 	name: 'fade scene',
+	// 	colorTrigger: 'black',
+	// 	indent: 200,
+	// 	colorStart: '#75C695'
+	// }) // this requires a plugin
+	.addTo(controller);
+
+
+	var controller = new ScrollMagic.Controller();
+
+	// build a scene
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '#project04'
+	})
+	.setClassToggle('#project04', 'fade-in') // add class to project01
+	// .addIndicators({
+	// 	name: 'fade scene',
+	// 	colorTrigger: 'black',
+	// 	indent: 200,
+	// 	colorStart: '#75C695'
+	// }) // this requires a plugin
+	.addTo(controller);
+	
+
+});
+
+
+// $(document).ready(function(){
+
+// 	// Init ScrollMagic
+// 	var controller = new ScrollMagic.Controller();
+
+// 	// pin the intro
+// 	var pinIntroScene = new ScrollMagic.Scene({
+// 		triggerElement: '#intro',
+// 		triggerHook: 0,
+// 		duration: '30%'
+// 	})
+// 	.setPin('#intro', {pushFollowers: false})
+// 	.addTo(controller);
+
+// 	// pin again
+// 	var pinIntroScene2 = new ScrollMagic.Scene({
+// 		triggerElement: '#project01',
+// 		triggerHook: 0.4
+// 	})
+// 	.setPin('#intro', {pushFollowers: false})
+// 	.addTo(controller);
+
+// 	// parallax scene
+
+// 	var parallaxTl = new TimelineMax();
+// 	parallaxTl
+// 		.from('.content-wrapper', 0.4, {autoAlpha: 0, ease:Power0.easeNone}, 0.4)
+// 		.from('.bcg', 2, {y: '-50%', ease:Power0.easeNone}, 0)
+// 		;
+
+// 	var slideParallaxScene = new ScrollMagic.Scene({
+// 		triggerElement: '.bcg-parallax',
+// 		triggerHook: 1,
+// 		duration: '100%'
+// 	})
+// 	.setTween(parallaxTl)
+// 	.addTo(controller);
+
+
+// 	// loop through each .project element
+// 	$('.project').each(function(){
+
+// 		// build a scene
+// 		var ourScene = new ScrollMagic.Scene({
+// 			triggerElement: this.children[0],
+// 			triggerHook: 0.9
+// 		})
+// 		.setClassToggle(this, 'fade-in') // add class to project01
+// 		.addIndicators({
+// 			name: 'fade scene',
+// 			colorTrigger: 'black',
+// 			colorStart: '#75C695',
+// 			colorEnd: 'pink'
+// 		}) // this requires a plugin
+// 		.addTo(controller);
+
+// 	});
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
